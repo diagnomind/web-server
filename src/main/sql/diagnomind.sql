e1a8df9e540a054bf1d6f276f23d40340016836f
Eñaut Genua <enaut.genua@alumni.mondragon.edu>
2023-12-12 17:05:31 +0100

    Refactor SQL script

--- src/main/sql/diagnomind.sql
+++ src/main/sql/diagnomind.sql
@@ -1,63 +1,57 @@
-
 CREATE SCHEMA IF NOT EXISTS `Diagnomind` DEFAULT CHARACTER SET utf8;
 USE `Diagnomind`;
 
 # hospital taula
 create table if not exists `Diagnomind`.`hospital` (
-  gid 					bigint auto_increment,
-  name 					varchar(255),
-  subscription_plan 	int,
-  subscriptionStart 	date,
-  subscriptionEnd 		date,
+  gid               bigint auto_increment,
+  name              varchar(255),
+  subscription_plan int,
+  subscriptionStart date,
+  subscriptionEnd   date,
   primary key (gid))
 engine = InnoDB;
 
 create table if not exists `Diagnomind`.`user` (
-  uid 					bigint auto_increment,
-  `name` 				varchar(255),
-  surname 				varchar(255),
-  ssn 					varchar(255),
-  
-  hospital_gid 			bigint,
+  uid           bigint auto_increment,
+  `name`        varchar(255),
+  surname       varchar(255),
+  ssn           varchar(255),
+  hospital_gid  bigint,
   primary key (UID))
 engine = InnoDB;
 
 create table if not exists `Diagnomind`.`image` (
-  id 					bigint,
-  imageData 			mediumblob not null, -- blob pilla 32Mb
-  
-  request_id			bigint,
+  id          bigint,
+  imageData   mediumblob not null, -- blob pilla 32Mb
+  request_id  bigint,
   primary key(id)) 
 engine = InnoDB;
 
 create table if not exists `Diagnomind`.`feedback` (
-  id 					bigint,
-  isCorrectDiagnosis	bool,
-  
-  request_id			bigint,
+  id                  bigint,
+  isCorrectDiagnosis  bool,
+  request_id          bigint,
   primary key(id))
 engine = InnoDB;
 
 create table if not exists `Diagnomind`.`request` (
-  id 					bigint auto_increment,
-  `date` 				date,
-  
-  image_id 				varchar(255),
-  feedback_id 			bigint,
-  user_uid 				bigint,
+  id          bigint auto_increment,
+  `date`      date,
+  image_id    varchar(255),
+  feedback_id bigint,
+  user_uid    bigint,
   primary key (id))
 engine = InnoDB;
 
 create table if not exists `Diagnomind`.`TrainingData` (
-  id 					bigint,
+  id  bigint,
   primary key(id))
 engine = InnoDB;
 
 create table if not exists `Diagnomind`.`TrainingImage` (
-  id 					bigint,
-  isCorrectDiagnosis	bool,
-  
-  image_id				bigint,
-  trainingdata_id		bigint,
+  id                  bigint,
+  isCorrectDiagnosis  bool,
+  image_id            bigint,
+  trainingdata_id     bigint,
   primary key(id))
 engine = InnoDB;
