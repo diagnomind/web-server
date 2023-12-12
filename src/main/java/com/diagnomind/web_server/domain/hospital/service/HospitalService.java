d87fe84f6e0cef9c96affdf685113ae5e810c688
Eñaut Genua <enaut.genua@alumni.mondragon.edu>
2023-12-12 10:47:51 +0100

    Add hospital service and its tests skeleton

--- src/main/java/com/diagnomind/web_server/domain/hospital/service/HospitalService.java
+++ src/main/java/com/diagnomind/web_server/domain/hospital/service/HospitalService.java
@@ -4,17 +4,51 @@ import java.util.List;
 import java.util.Optional;
 
 import com.diagnomind.web_server.domain.hospital.model.Hospital;
+import com.diagnomind.web_server.domain.hospital.repository.HospitalRepository;
 import com.diagnomind.web_server.domain.user.model.User;
+import com.diagnomind.web_server.domain.user.repository.UserRepository;
 
-// Change it to a class
-public interface HospitalService {
-    void addUser(Integer gid, User user);
-    Optional<User> getUser(Integer gid, Integer uid);
-    List<User> getAllUsers(Integer gid);
-    Optional<User> modifyUser(User user);
-    void addHospital(Hospital hospital);
-    void getHospital(Integer gid);
-    List<Hospital> getAllHospitals();
-    Optional<Hospital> modifyHospital(Hospital hospital);
-    void deleteHospital(Integer gid);
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
+    private final UserRepository userRepository;
+
+    Optional<User> addUser(Integer gid, User user) {
+        
+    }
+
+    Optional<User> getUser(Integer gid, Integer uid) {
+
+    }
+
+    List<User> getAllUsers(Integer gid) {
+
+    }
+
+    Optional<User> modifyUser(User user) {
+
+    }
+
+    void addHospital(Hospital hospital) {
+
+    }
+
+    void getHospital(Integer gid) {
+
+    }
+
+    List<Hospital> getAllHospitals() {
+
+    }
+
+    Optional<Hospital> modifyHospital(Hospital hospital) {
+
+    }
+
+    void deleteHospital(Integer gid) {
+
+    }
 }
