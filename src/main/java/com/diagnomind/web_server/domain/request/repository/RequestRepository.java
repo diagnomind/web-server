4fd0695ac7b7964cb002dfe3346737534328ff80
Eñaut Genua <enaut.genua@alumni.mondragon.edu>
2023-12-13 21:07:34 +0100

    Delete unnecessary repositories

--- src/main/java/com/diagnomind/web_server/domain/request/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diagnomind.web_server.domain.request.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.diagnomind.web_server.domain.request.model.Request;
-
-public interface RequestRepository extends CrudRepository<Request, Integer> {
-    
-}
